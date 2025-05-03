2ae5cda2232d3d09b28ac3df8c3f17a6ed42eb02
saki12-05 <notice1205@gmail.com>
2025-05-03 09:46:41 +0900

    kadai_007をプッシュ

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang=ja>
+    
+<head>
+    <meta charset = "utf-8">
+    <title>PHP基礎編_課題7</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $vegetables = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+        print_r($vegetables);
+        ?>
+    </p>
+
+</body>
+
+</html>
